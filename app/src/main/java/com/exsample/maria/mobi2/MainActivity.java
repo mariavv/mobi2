160ead031425d52527fb3ac512d443f7182afeea
vasilevamariav <vasilevamariav@yandex.ru>
2018-02-14 19:19:50 +0400

    auth

--- app/src/main/java/com/exsample/maria/mobi2/MainActivity.java
+++ app/src/main/java/com/exsample/maria/mobi2/MainActivity.java
@@ -3,11 +3,22 @@ package com.exsample.maria.mobi2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SING_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
+                RC_SING_IN);
     }
 }
